b38d0aaad2a55d3be3d0a5af04b5e094888e3e7e
allen612017 <56889929+allen612017@users.noreply.github.com>
2019-11-05 19:04:07 +0000

    Testing DirectoryInfo.GetFiles()
    Change log
    05.11.19 - We researched how to use the method:DirectoryInfo.GetFiles() in order to retreive a list of all the files in a directory and spent some time getting 
    	   familiar with how to use it.

--- Program.cs
+++ Program.cs
@@ -1,15 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace Browser
 {
     class Program
-    {
+    {// Testing the getfiles method and getting familiar with how it can be used in my project
         static void Main(string[] args)
         {
+            DirectoryInfo di = new DirectoryInfo(@"C:\Users\allen\Desktop");
+            Console.WriteLine("No search pattern returns:");
+            foreach (var fi in di.GetFiles())
+            {
+                Console.WriteLine(fi.Name);
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
         }
     }
 }
